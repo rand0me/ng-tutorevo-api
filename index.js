6e8b92a84cd4ea0a4626632181a0353a2ca78584
rand0me <not.randome@gmail.com>
2017-06-20 02:39:43 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const {Server} = require('http');
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const http = new Server(app);
+const io = socket(http);
+
+const User = require('./lib/user').User;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+io.on('connection', socket => new User(socket, io));
+
+http.listen(3000, () => console.log('listening on *:3000'));
