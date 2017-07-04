31f3ba068bc8175a0462d00acc774c10f9ebbbb8
Mark Bober <beavermark93@gmail.com>
2017-07-05 02:58:18 +0300

    Added endpoints for meetings

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const http = new Server(app);
 const io = socket(http);
 
 const User = require('./lib/user').User;
+const Meeting = require('./lib/meeting').Meeting;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 io.on('connection', socket => new User(socket, io));
+
+io.on('connection', socket => new Meeting(socket, io));
 
 http.listen(3000, () => console.log('listening on *:3000'));
